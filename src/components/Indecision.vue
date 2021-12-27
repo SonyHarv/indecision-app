75a13fd971d0c1344e3b97dd7a49f35212737bcc
Erikson TF <sonyharv9@gmail.com>
2021-12-27 10:37:54 -0800

    feat(components): Add Indecision component

---
+++ src/components/Indecision.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Hola desde Indecision Component</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Indecision",
+};
+</script>
+<style>
+h2 {
+  color: palevioletred;
+}
+</style>

9f6a15df07ef415b1a7772b532b1d80aa1c8a7ae
Erikson TF <sonyharv9@gmail.com>
2021-12-27 16:22:48 -0800

    feat(components): Add data,watch,structure question

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    <h2>Hola desde Indecision Component</h2>
+  <div class="question">
+    <input
+      type="text"
+      title="Introduce tu duda"
+      placeholder="Ella me ama?"
+      v-model="question"
+    />
+    <h2>Recuerda terminar con un signo de interrogación(?)</h2>
+  </div>
+  <div class="questionWatch">
+    <p>{{ question }}</p>
   </div>
 </template>
 <script>
 export default {
   name: "Indecision",
+
+  data() {
+    return {
+      question: null,
+    };
+  },
+  watch: {
+    question(value, oldValue) {
+      // console.log({ value, oldValue });
+      if (!value.includes("?")) {
+        console.log("entro aqui");
+      } else {
+        console.log("Hola");
+      }
+    },
+  },
 };
 </script>
 <style>
 h2 {
-  color: palevioletred;
+  color: #f0eff0;
+  font-size: 1.2rem;
+}
+input {
+  border: none;
+  border-radius: 5px;
+  /* block-size: 1.5rem; */
+  inline-size: 350px;
+  padding-block-start: 13px;
+  padding-block-end: 13px;
+  padding-inline-start: 1rem;
+  padding-inline-end: 1rem;
+  outline: none;
+}
+input[type="text"]:focus {
+  border: 2px solid #0086df;
+}
+.questionWatch p {
+  color: white;
 }
 </style>

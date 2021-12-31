0128184c780afc4b36e08762b3ad6493ef4db28a
Erikson TF <sonyharv9@gmail.com>
2021-12-30 21:50:26 -0800

    test(unit): Add expect and tobeGreaterThan

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,14 +3,17 @@ describe("Example Component", () => {
   //Test
   test("Debe de ser mayor a 10", () => {
     //Arreglar
-    let value = 5;
+    let value = 10;
     //Estimulación
     value = value + 2;
     //Observar el resultado(aserciones)
-    if (value > 10) {
+    /*  if (value > 10) {
       // TODO:Todo bien
     } else {
       throw `${value} no es mayor a 10`;
-    }
+    } */
+
+    ///Asercion con expect y toBeGreaterThan
+    expect(value).toBeGreaterThan(10);
   });
 });
